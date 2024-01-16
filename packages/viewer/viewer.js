db2f5c0e9bd5faad5826c8c9117a6bf68cb0cefb
bhanuka.chathuranga <bhanuka.chathuranga@thenextimmoscoop.be>
2024-01-16 12:31:20 +0530

    fix(viewer): fix the value

--- packages/viewer/viewer.js
+++ packages/viewer/viewer.js
@@ -3,7 +3,7 @@ const iseven = require('@vh/iseven');
 const isodd = require('@vh/isodd');
 
 (function viewer() {
-  console.log(iseven());
-  console.log(isodd());
+  console.log(iseven(5));
+  console.log(isodd(5));
   return;
 })();
