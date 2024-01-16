37f26036aaeecca1c3a4a5b921296a7f4a5a984c
bhanuka.chathuranga <bhanuka.chathuranga@thenextimmoscoop.be>
2024-01-16 11:44:01 +0530

    feat(isViewer): isViewer package added

---
+++ packages/viewer/viewer.js
@@ -0,0 +1,9 @@
+'use strict';
+const iseven = require('@vh/iseven');
+const isodd = require('@vh/isodd');
+
+(function viewer() {
+  console.log(iseven());
+  console.log(isodd());
+  return;
+})();
